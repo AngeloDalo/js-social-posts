6e75439ae15346b8c067482bb3e32630c29f51a3
AngeloDalo <angelo251158@gmail.com>
2021-12-10 13:40:49 +0100

    object

---
+++ main.js
@@ -0,0 +1,29 @@
+//- Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: nome autore, foto profilo, data in formato americano, testo del post, immagine (non tutti i post devono avere una immagine), numero di likes.
+//- Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
+
+const social = [
+    {
+        name: 'Phil Mangione',
+        time: '4 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        profilePic: 'https://unsplash.it/300/300?image=15',
+        postImage: 'https://unsplash.it/600/300?image=171',
+        like: 80,
+    },
+    {
+        name: 'Sofia Perlari',
+        time: '2 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        profilePic: 'https://unsplash.it/300/300?image=16',
+        postImage: 'https://unsplash.it/600/300?image=172',
+        like: 91,
+    },
+    {
+        name: 'George Lucas',
+        time: '1 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        profilePic: 'https://unsplash.it/300/300?image=12',
+        postImage: 'https://unsplash.it/600/300?image=173',
+        like: 102,
+    },
+  ];
